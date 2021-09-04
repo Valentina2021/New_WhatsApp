62aeec564dfb5d572c943a04b11396e2b8683dab
Valentina <est.valentina.alvac@unimilitar.edu.co>
2021-09-04 12:38:28 -0500

    Chatlist created

--- components/ChatListItem/style.tsx
+++ components/ChatListItem/style.tsx
@@ -1,6 +1,36 @@
 import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
-    
+    container:{
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    leftContainer:{
+        flexDirection: 'row',
+    },
+    avatar:{
+        width: 55,
+        height: 55,
+        marginHorizontal: 9,
+        borderRadius: 40,
+    },
+    username:{
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+    lastMessages:{
+        fontSize:15,
+        color: 'grey',
+    },
+    time:{
+        fontSize:7,
+        color: 'black',
+    },
+    midContainer:{
+        justifyContent: 'space-around',
+    }
+
 });
 
 export default styles;
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import {  View } from '../components/Themed';
+import { RootTabScreenProps } from '../types'
+import ChatListItem from '../components/ChatListItem';
+
+import chatRooms from '../data/ChatRooms';
 
 export default function ChatScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-    
+      <FlatList
+      data={chatRooms}
+      renderItem={({item}) => <ChatListItem chatRoom={item}/>}
+      keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
